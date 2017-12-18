1932fab966f1a4637a841344c1d628163fdb0dd3
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-18 19:47:09 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
     Show.minimum(:rating)
   end
 
-  def least_popular_show
+  def self.least_popular_show
     show = Show.find_by(rating: lowest_rating)
     show
   end
